222b5ca633ddd88a6df576913900393554927cab
kimjw2003 <kimjw111003@gmail.com>
2021-02-27 00:05:38 +0900

    [ADD] Intro

---
+++ app/src/main/java/com/example/food_with_tts/IntroActivity.kt
@@ -0,0 +1,23 @@
+package com.example.food_with_tts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+
+        Handler().postDelayed({
+            startActivity(Intent(this@IntroActivity, StartActivity::class.java))
+                    finish()
+        },1000)
+
+    }
+}
